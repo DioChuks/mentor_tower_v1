dfa42d01290370683ee88cb32b5ad0444dfbb877
DioChuks <diochuks65@gmail.com>
2024-07-09 20:01:58 +0100

    imports again

--- api/routes/_auth.routes.ts
+++ api/routes/_auth.routes.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
-import validate  from '@/app/validations/validate/_validate.middleware';
+import validate from "@/app/validations/validate/_validate.middleware";
 import * as authValidation from "@/app/validations/auth";
-import IsAuth from '@/app/middleware/auth';
+import IsAuth from "@/app/middleware/auth";
 
 import * as authController from "@/app/controllers/_auth.controller"
 

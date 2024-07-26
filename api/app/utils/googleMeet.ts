d7d59529a9dd9d0371f0406d50cab9c3bc4f7956
DioChuks <diochuks65@gmail.com>
2024-07-26 13:58:56 +0100

    fix: google insert issues using the types

--- api/app/utils/googleMeet.ts
+++ api/app/utils/googleMeet.ts
@@ -1,15 +1,10 @@
 import { calendar } from '../config/google';
+import { calendar_v3 } from 'googleapis';
 
 const generateMeetLink = async (): Promise<string> => {
-  const event = {
+  const event: calendar_v3.Schema$Event = {
     summary: 'Meeting',
     description: 'Mentorship Meeting',
-    conferenceData: {
-      createRequest: {
-        requestId: 'some-random-string',
-        conferenceSolutionKey: { type: 'hangoutsMeet' }
-      }
-    },
     start: {
       dateTime: new Date().toISOString(),
       timeZone: 'America/Los_Angeles'
@@ -17,12 +12,18 @@ const generateMeetLink = async (): Promise<string> => {
     end: {
       dateTime: new Date(new Date().getTime() + 60 * 60 * 1000).toISOString(),
       timeZone: 'America/Los_Angeles'
+    },
+    conferenceData: {
+      createRequest: {
+        requestId: 'some-random-string',
+        conferenceSolutionKey: { type: 'hangoutsMeet' }
+      }
     }
   };
 
   const response = await calendar.events.insert({
     calendarId: 'primary',
-    resource: event,
+    requestBody: event,
     conferenceDataVersion: 1
   });
 
